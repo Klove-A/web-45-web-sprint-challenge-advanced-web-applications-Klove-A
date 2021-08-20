457540cd87e473bea2637222ecd28f0ff93a5d8c
Klove Adams <kalebsxott@gmail.com>
2021-08-20 14:47:17 -0400

    id's added still working on state and form in Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -16,7 +16,7 @@ const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   // const login = e => {
-  //   e.preventDefault();
+  //   e.preventDefault("http://localhost:5000/api/login", credentials);
   //   axios.post("")
   //     .then(res => {
 
@@ -45,7 +45,7 @@ const Login = () => {
             value={password}
             onChange={handleChange()}
           />
-          <button>Log in</button>
+          <button id="submit">Log in</button>
         </form>
       </div>
 
